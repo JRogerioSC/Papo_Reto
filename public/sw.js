ddb6ff5f03db90f5826036950e89727c95c2c11b
JRogerioSC <jo-rogerio@hotmail.com>
2025-06-18 21:40:47 -0300

    xxx

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,25 @@
+self.addEventListener('push', event => {
+  let data = { title: 'Default Title', body: 'Default body' }
 
-self.addEventListener('push', function(event) {
-    const data = event.data ? event.data.text() : 'No payload';
+  try {
+    if (event.data) {
+      data = event.data.json()
+    }
+  } catch (e) {
+    // Fallback: treat it as plain text
+    data = {
+      title: 'Notification',
+      body: event.data.text()
+    }
+  }
 
-    event.waitUntil(
-        data.then(text => {
-            self.registration.showNotification('Push Received', {
-                body: text,
-                icon: 'menssagensNews.png'
-            });
-        })
-    );
-});
+  event.waitUntil(
+    Promise.resolve(data.body).then(body => {
+      self.registration.showNotification(data.title, {
+        body: body,
+        icon: '/menssagensNews.png'
+      })
+    })
+  )
+})
+
