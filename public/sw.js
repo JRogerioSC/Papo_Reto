ad370334b6ab390fde51df26989b59daadc894c6
JRogerioSC <jo-rogerio@hotmail.com>
2025-06-19 20:37:31 -0300

    mo

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,18 @@
-self.addEventListener('push', event => {
-  const data = event.data?.json()
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+
+  const urlToOpen = event.notification.data?.url || '/';
+
   event.waitUntil(
-    self.registration.showNotification(data.title, {
-      body: data.body,
-      icon: '../../assets/icon-Notificacao.png'
-    })
-  )
-})
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(clientList => {
+      for (const client of clientList) {
+        if (client.url === urlToOpen && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow(urlToOpen);
+      }
+    })
+  );
+});
