c77d689140c52dd59422110694cdbe65863299d4
JRogerioSC <jo-rogerio@hotmail.com>
2025-06-20 00:41:02 -0300

    hlkh´t

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 // service-worker.js
 
 self.addEventListener('push', function(event) {
-  let data = { title: 'Nova Menssagem!', body: '', icon: 'https://cdn.pixabay.com/photo/2017/08/06/22/01/banner-2598287_1280.jpg', data: {} };
+  let data = { title: 'Nova Menssagem!', body: '', icon: 'https://cdn-icons-png.flaticon.com/512/484/484662.png', data: {} };
 
   if (event.data) {
     data = JSON.parse(event.data.text());
@@ -9,7 +9,7 @@ self.addEventListener('push', function(event) {
 
   const options = {
     body: data.body,
-    icon: data.icon || 'https://cdn.pixabay.com/photo/2017/08/06/22/01/banner-2598287_1280.jpg', // Ícone padrão, se não enviado
+    icon: data.icon || 'https://cdn-icons-png.flaticon.com/512/484/484662.png', // Ícone padrão, se não enviado
     data: {
       url: data.data?.url || 'https://pap0reto.netlify.app/' // URL para abrir ao clicar
     }
