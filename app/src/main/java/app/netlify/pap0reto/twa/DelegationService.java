3f12d20bffa44274898f47a38fa3cf77882dda5c
JRogerioSC <jo-rogerio@hotmail.com>
2025-06-22 20:53:56 -0300

    atManifest

---
+++ app/src/main/java/app/netlify/pap0reto/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.netlify.pap0reto.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
