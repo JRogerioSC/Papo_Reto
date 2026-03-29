c4b50ff74e48ae451c51e3eea7faeb3f369385d1
Rogerio <jo-rogerio@hotmail.com>
2026-03-29 17:57:34 -0300

    ujrhrh

--- vite.config.js
+++ vite.config.js
@@ -7,14 +7,20 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'logo192.png', 'logo512.png'], // garante que os ícones sejam incluídos
       manifest: {
         name: 'Papo Reto',
         short_name: 'Papo Reto',
-        start_url: '/',
-        display: 'standalone',
+        start_url: '.',               // melhor que '/' para TWA, evita mostrar a barra de URL
+        display: 'standalone',       // abre como app sem barra de navegador
         background_color: '#000000',
         theme_color: '#000000',
         icons: [
+          {
+            src: '/logo192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
           {
             src: '/logo512.png',
             sizes: '512x512',
